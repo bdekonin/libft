6f1d166a420efb828cf2c8c889fc630cc0adc352
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-23 12:54:11 +0200

    now using pointers

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,50 +6,34 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/02 14:18:39 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/10 20:44:20 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/23 10:32:05 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-** It first looks if the string begins with any specific,
-** characters and skips those.
-** Then it looks if the number is negative or positive. If it is negative it,
-** will change the check to a negative number so it can multiple it by a neg,
-** in the end.
-** Afterwards is looks if the string index is between 0 and 9 and devides,
-** then it does an ans multiple by 10 so it goes up one if the character has,
-** been converted on line number 47. and then it returns that value multiply.
-** by the negative check.
-** on line 45 till 49 it checks if the number can overflow.
-** it sets the number to the right number chosen by the check.
-*/
-
 int	ft_atoi(const char *str)
 {
 	long long int ans;
 	long long int check;
-	long long int i;
 
 	ans = 0;
 	check = 1;
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-')
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-')
 		check = -1;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	while (ft_isdigit(str[i]))
+	if (*str == '+' || *str == '-')
+		str++;
+	while (ft_isdigit(*str))
 	{
-		if (ans * 10 + str[i] - 48 < ans)
+		if (ans * 10 + *str - 48 < ans)
 		{
 			ans = (-1 * check - 1) / 2;
 			return (ans * check);
 		}
-		ans = (ans * 10) + str[i] - 48;
-		i++;
+		ans = (ans * 10) + *str - 48;
+		str++;
 	}
 	return (ans * check);
 }
