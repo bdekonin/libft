247b0aa1ed687b0d3ab2f24d69426c3dbef3348f
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-23 12:57:07 +0200

    added extra free function

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,22 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/14 16:18:24 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/10 20:47:04 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/23 11:49:52 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*freee(char **arr, int y)
+{
+	while (y > 0)
+	{
+		y--;
+		free(arr[y]);
+	}
+	free(arr);
+	return (NULL);
+}
 
 /*
 ** counts characters of the word
@@ -30,44 +41,6 @@ static int	checkstring(char *s, char c, int i)
 }
 
 /*
-** It counts how many words there are in a string.
-** It first checks if it sees a delimter, if so it adds one to the index and,
-** restarts the while loop. once the first if statement is false it checks,
-** if it sees a character different then the delimimter then counts that.
-** and saves it in J. once a j has been counted it goes to check if it is,
-** a word. if it is it resets J and counts the count up by one.
-*/
-
-static int	ft_word_count(char *s, char c)
-{
-	int i;
-	int count;
-	int j;
-
-	i = 0;
-	count = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			i++;
-			continue;
-		}
-		if (s[i] != c)
-			j++;
-		if ((j > 1 && s[i + 1] == c) || (j > 1 && s[i + 1] == '\0') ||
-(j == 1 && s[i + 1] == c) || (j == 1 && s[i + 1] == '\0'))
-		{
-			j = 0;
-			count++;
-		}
-		i++;
-	}
-	return (count);
-}
-
-/*
 ** x = Length of the string where the index is now located.
 ** y = array counter.
 */
@@ -79,19 +52,14 @@ static char	**splitting(char const *s, char c, int i, char **arr)
 
 	x = 0;
 	y = 0;
-	while (y != ft_word_count((char*)s, c))
+	while (y != ft_wordcount((char*)s))
 	{
 		if (s[i] != c)
 		{
 			x = checkstring((char*)s, c, i);
 			arr[y] = ft_substr((char*)s, i, x);
 			if (!arr[y])
-			{
-				while (y > 0)
-					free(arr[--y]);
-				free(arr);
-				return (0);
-			}
+				freee(arr, y);
 			i = i + x;
 			y++;
 		}
@@ -113,7 +81,7 @@ char		**ft_split(char const *s, char c)
 	i = 0;
 	if (!s)
 		return (NULL);
-	arr = (char**)malloc(sizeof(char*) * (ft_word_count((char*)s, c) + 1));
+	arr = (char**)malloc(sizeof(char*) * (ft_wordcount((char*)s) + 1));
 	if (arr == NULL)
 		return (NULL);
 	arr = splitting((char*)s, c, i, arr);
