df863de777b2a7fab112e44670bcf1431cda14cd
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-23 13:00:59 +0200

    returns write value

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,17 +6,23 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/01 16:25:38 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/10 20:46:42 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/23 13:00:38 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putendl_fd(char *s, int fd)
+int	ft_putendl_fd(char *s, int fd)
 {
-	if (s == 0)
+	int ret;
+
+	if (!s)
 		return ;
-	ft_putstr_fd(s, fd);
-	write(fd, "\n", 1);
+	ret = ft_putstr_fd(s, fd);
+	if (write(fd, "\n", 1) < 0)
+		ret = -1;
+	else
+		ret += 1;
+	return (ret);
 }
