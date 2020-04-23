e5f5b93ab07314d4bc17a1550cc976ff66420bc1
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-23 13:09:32 +0200

    returns value of write

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/01 16:25:38 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/23 13:00:38 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/23 13:01:39 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_putendl_fd(char *s, int fd)
 	int ret;
 
 	if (!s)
-		return ;
+		return (-1);
 	ret = ft_putstr_fd(s, fd);
 	if (write(fd, "\n", 1) < 0)
 		ret = -1;
