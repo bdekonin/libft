3651d9c4f2e31de469e92a5df0c3bbf283d7a218
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-26 21:28:23 +0200

    memcpy

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/07 11:09:27 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/26 20:48:17 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/26 20:51:53 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		len;
 
-	len = ft_strlen(s1);
 	if (!s1 || !s2)
 		return (NULL);
-	str = ft_calloc((len + ft_strlen(s2) + 1), sizeof(char));
+	str = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1, len);
-	ft_memcpy(str + len, s2, ft_strlen(s2));
+	ft_memcpy(str, s1, ft_strlen(s1));
+	ft_memcpy(str + ft_strlen(s1), s2, ft_strlen(s2));
 	return (str);
 }
