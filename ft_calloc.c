e56358a8455a249c44bda0a9381050b869913838
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-26 21:28:14 +0200

    memset

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/05 09:01:26 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/23 10:33:16 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/26 20:50:30 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,6 @@ void	*ft_calloc(size_t count, size_t size)
 	str = malloc(sizeof(char) * (count * size));
 	if (!str)
 		return (NULL);
-	else
-	{
-		while (i != (count * size))
-		{
-			str[i] = 0;
-			i++;
-		}
-		return (str);
-	}
+	ft_memset(str, 0, count * size);
+	return (str);
 }
