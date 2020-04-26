0ffe3656721e9860333ac16a1c584cd9c6a178c5
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-26 20:48:50 +0200

    strjoin update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/07 11:09:27 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/23 12:41:38 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/26 20:48:17 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,15 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		i;
-	int		j;
+	int		len;
 
-	i = 0;
-	j = 0;
+	len = ft_strlen(s1);
 	if (!s1 || !s2)
 		return (NULL);
-	str = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	str = ft_calloc((len + ft_strlen(s2) + 1), sizeof(char));
 	if (!str)
 		return (NULL);
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
+	ft_memcpy(str, s1, len);
+	ft_memcpy(str + len, s2, ft_strlen(s2));
 	return (str);
 }
